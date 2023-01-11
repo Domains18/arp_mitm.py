b0db1fffd0afed4f7f2564d65b8d6bcde5d0460c
Domains18 <gibsonsgibson88@gmail.com>
2023-01-11 19:47:40 +0300

    capture packets

--- index.py
+++ index.py
@@ -4,3 +4,27 @@ import sys
 import time
 import os
 from ipaddress import IPv4Address
+import threading
+
+# working dir
+cwd = os.getcwd()
+def sudoCheck():
+    if not "SUDO_UID" in os.environ.keys():
+        print("Acces Denied, Are you root?")
+        exit(1)
+        
+def arpScan(ipRange):
+    arpResponses = list()
+    answeredList = scapy.arping(ipRange, verbose=0)[0]
+    for res in answeredList:
+        arpResponses.append({"ip" : res[1].prsc, "mac" : res[1].hwsrc})
+    return arpResponses
+
+def isGateway(gatewayIp):
+    result = subprocess.run(["route", "-n"], captureOutput=True).stdout.decode().split("\n")
+    for row in result:
+        if gatewayIp in row:
+            return True 
+    return False
+   
+    
